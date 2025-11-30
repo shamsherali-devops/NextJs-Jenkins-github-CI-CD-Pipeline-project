801c1b78995c79092a33d8860882361a4bf02b5a
shamsherali-devops <shamsheralidev@gmail.com>
2025-11-30 18:05:38 +0500

    ci/cd pipeline

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Moving Abroad Made Easy</title>
+        <title> Moving-Abroad-Made-Easy  </title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Professional relocation and moving abroad services." />
         <link rel="icon" href="/favicon.ico" />
